f4cfbe272e15d434a6ff526f59fff7da49b9f769
Chris McDonald <mail.chris.mcdonald@gmail.com>
2015-08-09 04:21:47 -0600

    Updated solution to 2 with closed-form fibonacci solution

--- 2/2.py
+++ 2/2.py
@@ -1,15 +1,16 @@
+import math
+
 total = 0
 index = 0
 current_fibonacci = 0
 upper_limit = 4000000
 
 def Fibonacci(param):
-	if param == 1:
-		return 1
-	if param == 2:
-		return 2
-	return Fibonacci(param-1) + Fibonacci(param-2)
-		
+	A = (1 + math.sqrt(5))/2
+	B = (1 - math.sqrt(5))/2
+	temp = (A**param - B**param) / math.sqrt(5)
+	return int(round(temp))	
+	
 while(current_fibonacci < upper_limit):
 	if current_fibonacci % 2 == 0:
 		total += current_fibonacci
