f8ea9d3e76fc2ab7989ca4a155872403857c76c3
Chris McDonald <mail.chris.mcdonald@gmail.com>
2015-08-12 04:26:41 -0600

    Added data to 11 and first functions

--- 11/11.py
+++ 11/11.py
@@ -1,3 +1,21 @@
-GRID_DIMENSION = 20
+GRID_DIMENSION = 20 #20x20
+PRODUCT_LENGTH = 4
+grid = []
 
-with open('grid.txt') as data
+#We only need to search in four directions due to symmetry: Down-Left, Down, Down-Right, Right
+
+def RightProduct(input_array, row, column):
+	product = 1
+	if column + 4 >= GRID_DIMENSION:
+		return 0
+	else:
+		for x in range(PRODUCT_LENGTH):
+			product *= input_array[row][column + x] 
+		return product
+
+def DownProduct(input_array, row, column):
+with open('grid.txt') as data:
+	for line in data:
+		grid.append(line.split())
+
+print grid[19][20]
