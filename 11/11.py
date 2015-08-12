f65bb2bd2b28740c270c897b6c957fa2c3019ad6
Chris McDonald <mail.chris.mcdonald@gmail.com>
2015-08-12 08:31:05 -0600

    Solution to 11

--- 11/11.py
+++ 11/11.py
@@ -1,7 +1,8 @@
 GRID_DIMENSION = 20 #20x20
 PRODUCT_LENGTH = 4
 
-#We only need to search in four directions due to symmetry: Down-Left, Down, Down-Right, Right
+#We only need to search in four directions due to symmetry: Down-Left, Down, Down-Right, and Right
+
 
 def RightProduct(input_array, row, column):
 	product = 1
@@ -12,6 +13,7 @@ def RightProduct(input_array, row, column):
 			product *= input_array[row][column + x] 
 		return product
 
+
 def DownProduct(input_array, row, column):
 	product = 1
 	if row + PRODUCT_LENGTH >= GRID_DIMENSION:
@@ -20,6 +22,7 @@ def DownProduct(input_array, row, column):
 		for x in range(PRODUCT_LENGTH):
 			product *= input_array[row + x][column]
 		return product
+
 
 def DownRightProduct(input_array, row, column):
 	product = 1
@@ -32,10 +35,45 @@ def DownRightProduct(input_array, row, column):
 			product *= input_array[row + x][column + x]
 		return product
 
-def DownLeftProduct(input_array, row, column)
+
+def DownLeftProduct(input_array, row, column):
+	product = 1
+	if column < 3:
+		return 0
+	elif row + PRODUCT_LENGTH >= GRID_DIMENSION:
+		return 0
+	else:
+		for x in range(PRODUCT_LENGTH):
+			product *= input_array[row + x][column - x]
+		return product
+
+def MaxDirectionalProduct(input_array, row, column):
+	MaxProduct = 0
+	MaxProduct = max(RightProduct(input_array, row, column), MaxProduct)
+	MaxProduct = max(DownProduct(input_array, row, column), MaxProduct)
+	MaxProduct = max(DownRightProduct(input_array, row, column), MaxProduct)
+	MaxProduct = max(DownLeftProduct(input_array, row, column), MaxProduct)
+	return MaxProduct
+
 
 with open('data.txt') as data:
+
+#Get our data in to a nice array
+
 	grid = []
 	for line in data:
 		grid.append(line.split())
-	print map(max, grid) 
+
+#Make sure all of the data is of the right type
+	for row in range(GRID_DIMENSION):
+		for column in range(GRID_DIMENSION):
+			grid[row][column] = int(grid[row][column])
+	
+#Run each product over each entry in the array. The functions themselves take care of the array boundaries.		
+
+	LargestProduct = 0
+	for row in range(GRID_DIMENSION):
+		for column in range(GRID_DIMENSION):
+			LargestProduct = max(LargestProduct, MaxDirectionalProduct(grid, row, column))
+
+	print LargestProduct	
