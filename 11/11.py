89448d10440c36397f5c8a21713b7236c4950981
Chris McDonald <mail.chris.mcdonald@gmail.com>
2015-08-12 05:15:06 -0600

    Cleaned up structure of 11

--- 11/11.py
+++ 11/11.py
@@ -1,12 +1,11 @@
 GRID_DIMENSION = 20 #20x20
 PRODUCT_LENGTH = 4
-grid = []
 
 #We only need to search in four directions due to symmetry: Down-Left, Down, Down-Right, Right
 
 def RightProduct(input_array, row, column):
 	product = 1
-	if column + 4 >= GRID_DIMENSION:
+	if column + PRODUCT_LENGTH >= GRID_DIMENSION:
 		return 0
 	else:
 		for x in range(PRODUCT_LENGTH):
@@ -14,8 +13,29 @@ def RightProduct(input_array, row, column):
 		return product
 
 def DownProduct(input_array, row, column):
-with open('grid.txt') as data:
+	product = 1
+	if row + PRODUCT_LENGTH >= GRID_DIMENSION:
+		return 0
+	else:
+		for x in range(PRODUCT_LENGTH):
+			product *= input_array[row + x][column]
+		return product
+
+def DownRightProduct(input_array, row, column):
+	product = 1
+	if row + PRODUCT_LENGTH >= GRID_DIMENSION:
+		return 0
+	elif column + PRODUCT_LENGTH >= GRID_DIMENSION:
+		return 0
+	else:
+		for x in range(PRODUCT_LENGTH):
+			product *= input_array[row + x][column + x]
+		return product
+
+def DownLeftProduct(input_array, row, column)
+
+with open('data.txt') as data:
+	grid = []
 	for line in data:
 		grid.append(line.split())
-
-print grid[19][20]
+	print map(max, grid) 
