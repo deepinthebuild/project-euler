2415e92d16a54eb40d03c2574987915cd3645ecc
Chris McDonald <mail.chris.mcdonald@gmail.com>
2015-08-13 10:31:02 -0600

    Solution to 18

---
+++ 18/18.py
@@ -0,0 +1,27 @@
+def PairwiseMaximum(input_list):
+	output_list = []
+	for x in range(len(input_list) - 1):
+		output_list.append(max(input_list[x], input_list[x + 1]))
+	return output_list
+
+#Crunches the bottom row in the row above it
+def TriangleCollapse(list_of_lists):
+	working_entry = list_of_lists
+	bottom = working_entry.pop()
+	second_to_bottom = working_entry.pop()
+	new_entry = [sum(x) for x in zip(second_to_bottom, PairwiseMaximum(bottom))]
+	working_entry.append(new_entry)
+	return working_entry
+
+with open('data.txt') as data:
+
+#import data and set it to the right type
+	triangle = []
+	for line in data:
+		triangle.append(line.split())
+	triangle = [[int(k) for k in line] for line in triangle]
+
+	while(len(triangle) != 1):
+		triangle = TriangleCollapse(triangle)
+
+	print triangle
