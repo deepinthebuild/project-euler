b9ae925a54f67020d195cd69ee9ce4a2f4b26e50
Chris McDonald <mail.chris.mcdonald@gmail.com>
2015-08-13 12:07:37 -0600

    Solution to 22

---
+++ 22/22.py
@@ -0,0 +1,26 @@
+
+
+def LetterScore(character):
+	return ord(character) - 64
+
+
+def WordScore(word):
+	character_list = list(word)
+	score = 0
+	for letter in character_list:
+		score += LetterScore(letter)
+	return score
+
+with open('names.txt') as data:
+	names = data.read().split(',')
+	names = [entry.strip('\"') for entry in names]
+	names = [entry.upper() for entry in names]
+	names.sort()
+
+total_score = 0
+name_scores = []
+
+for position, name in enumerate(names):
+	name_scores.append(WordScore(name) * (position + 1))
+
+print sum(name_scores)
